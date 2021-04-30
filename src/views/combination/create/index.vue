f27ebd4e34a989ff1edf46ef8899e807dd08168c
Tshirt-hfk <fanglei212@outlook.com>
2021-04-30 14:56:24 +0800

    样例表单验证完成

--- src/views/combination/create/index.vue
+++ src/views/combination/create/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="名称">
+    <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+      <el-form-item label="样例名称" prop="name">
         <el-input v-model="form.name" />
       </el-form-item>
-      <el-form-item label="预处理模型">
+      <el-form-item label="预处理模型" prop="pre_model">
         <el-select
           v-model="form.pre_model"
           filterable
@@ -23,9 +23,9 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="翻译模型">
+      <el-form-item label="翻译模型" prop="trans_model">
         <el-select
-          v-model="form.trans_model" 
+          v-model="form.trans_model"
           filterable
           remote
           reserve-keyword
@@ -42,7 +42,7 @@
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="描述">
+      <el-form-item label="描述" prop="desc">
         <el-input v-model="form.desc" type="textarea" />
       </el-form-item>
       <el-form-item>
@@ -63,22 +63,63 @@ export default {
       trans_loading: true,
       pre_options: [],
       trans_options: [],
+
       form: {
         name: "",
-        pre_model: null,
-        trans_model: null,
+        pre_model: "",
+        trans_model: "",
         desc: "",
+      },
+
+      rules: {
+        name: [
+          { required: true, message: "请输入样例名称", trigger: "blur" },
+          {
+            min: 1,
+            max: 20,
+            message: "长度在 1 到 20 个字符",
+            trigger: "blur",
+          },
+        ],
+        pre_model: [
+          {
+            required: true,
+            message: "请选择样例的预处理模型",
+            trigger: "change",
+          },
+        ],
+        trans_model: [
+          {
+            required: true,
+            message: "请选择样例的翻译模型",
+            trigger: "change",
+          },
+        ],
+        desc: [
+          { required: true, message: "请输入样例描述", trigger: "blur" },
+          {
+            min: 1,
+            max: 200,
+            message: "长度在 1 到 200 个字符",
+            trigger: "blur",
+          },
+        ],
       },
     };
   },
   methods: {
     onSubmit() {
-      console.log(this.form)
-      createExample(this.form).then((response) => {
-        this.$message(response.message);
-        this.$router.push({ name: "example" })
-      })
-      this.$message("submit!");
+      this.$refs["form"].validate((valid) => {
+        if (valid) {
+          createExample(this.form).then((response) => {
+            this.$message(response.message);
+            this.$router.push({ name: "example" });
+          });
+          this.$message("submit!");
+        } else {
+          return false;
+        }
+      });
     },
 
     pre_remoteMethod(query) {
